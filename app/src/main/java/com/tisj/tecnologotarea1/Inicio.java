eae59c641c49ab74f88abb4249ae698c3ac9dffd
Nicolás <nicoflecap1@gmail.com>
2016-09-08 20:54:44 -0300

    dos ativitys

--- app/src/main/java/com/tisj/tecnologotarea1/Inicio.java
+++ app/src/main/java/com/tisj/tecnologotarea1/Inicio.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 public class Inicio extends AppCompatActivity {
@@ -14,7 +15,25 @@ public class Inicio extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio);
+
+        Button bttInsertarUsuario = (Button) findViewById(R.id.inicio_insertarUsuario);
+        bttInsertarUsuario.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Inicio.this,InsertarUsuario.class);
+                startActivity(intent);
+            }
+        });
+
+        Button bttListarUsuarios = (Button) findViewById(R.id.inicio_listarUsuario);
+        bttListarUsuarios.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Intent intent = new Intent(Inicio.this,ListarUsuarios.class);
+                startActivity(intent);
+            }
+        });
     }
+
+
 
     /** Called when the user clicks the Send button */
     public void sendMessage(View view) {
---
+++ app/src/main/java/com/tisj/tecnologotarea1/ListarUsuariosFragment.java
@@ -0,0 +1,22 @@
+package com.tisj.tecnologotarea1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class ListarUsuariosFragment extends Fragment {
+
+    public ListarUsuariosFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_listar_usuarios, container, false);
+    }
+}
